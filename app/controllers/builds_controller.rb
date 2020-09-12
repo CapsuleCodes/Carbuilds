4152543a0f5c6075df0b82af289990761f8cc33e
capsulecodes <capsulecodes@gmail.com>
2020-09-12 06:27:34 -0700

    update

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,3 +1,10 @@
 class BuildsController < ApplicationController
-    
+    get '/builds' do
+        if logged_in?
+            @builds = Build.all
+            erb :'builds/index'
+        else
+            erb:'users/login', locals: {message: "Log In First"}
+        end
+    end
 end
