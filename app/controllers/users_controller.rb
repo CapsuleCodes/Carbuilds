ffff499a6ecdcb940b9c81825298e45c00fc62ff
capsulecodes <capsulecodes@gmail.com>
2020-09-12 06:27:29 -0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,42 @@ class UsersController < ApplicationController
         end
     end
 
-    post '/signup' do
-        if params
+  post '/signup' do
+     if params.value.any? {|value| value == ""}
+         erb :'users/signup', locals: {message: "Unable to Proceed"}
+     else
+         @user = User.new(username: params[username], email: params[:email], password: params[:password])
+         @user.save
+         session[:user_id] = @user.user_id
+         redirect to '/builds'
+     end
+  end
 
+  get '/login' do 
+      if !logged_in?
+        erb: '/users/login'
+      else    
+        redirect to '/builds'
+      end
+  end 
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id 
+        redirect to '/builds'
+    else
+        erb :'users/login', locals: {message: "Username or password incorrect!"}
+    end
+  end
+
+
+    get '/logout' do
+        if session[:user.id] != nil
+            session.destroy
+            redirect to '/'
+        else
+            redirect to '/projects'
+        end
+    end
 end
